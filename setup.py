7f9f87b6a073037bd4c2f54a1611aea01e1196c5
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2016-01-05 13:44:11 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "hotdoc_gst_extension",
+    version = "0.6.5",
+    keywords = "gstreamer hotdoc",
+    url='https://github.com/hotdoc/hotdoc_gst_extension',
+    author_email = 'mathieu.duponchelle@opencreed.com',
+    license = 'LGPL',
+    description = "An extension for hotdoc that documents gstreamer plugins",
+    author = "Mathieu Duponchelle",
+    packages = find_packages(),
+
+    package_data = {
+        '': ['*.html'],
+    },
+
+    entry_points = {'hotdoc.extensions': 'get_extension_classes = hotdoc_gst_extension.gst_extension:get_extension_classes'},
+    install_requires = [
+        'hotdoc>=0.6.5',
+        'hotdoc_c_extension>=0.6.5',
+    ],
+)
